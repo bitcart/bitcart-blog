cbaf23b6dfb16aa105d05c37f65037ffbe3277d7
Tobias Christiansen <tobi@tobyase.de>
2020-10-03 22:40:48 +0200

    Fixed linter configuration to actually work

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,19 +2,25 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint",
   },
   extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended',
-    'plugin:prettier/recommended'
+    "@nuxtjs",
+    "plugin:nuxt/recommended",
+    "eslint:recommended",
+    "plugin:vue/recommended",
+    "plugin:prettier/recommended",
   ],
-  plugins: [],
+  plugins: ["vue"],
   // add your custom rules here
   rules: {
-    'prettier/prettier': ['error', { semi: false }]
-  }
+    semi: [2, "never"],
+    "no-console": "off",
+    "prettier/prettier": ["error", { semi: false }],
+    "vue/html-self-closing": 0,
+    "vue/singleline-html-element-content-newline": 0,
+  },
 }
--- nuxt.config.js
+++ nuxt.config.js
@@ -4,24 +4,24 @@ export default {
    ** Nuxt target
    ** See https://nuxtjs.org/api/configuration-target
    */
-  target: 'static',
+  target: "static",
   /*
    ** Headers of the page
    ** See https://nuxtjs.org/api/configuration-head
    */
   head: {
-    title: 'BitcartCC Blog',
-    titleTemplate: '%s - BitcartCC Official Blog',
+    title: "BitcartCC Blog",
+    titleTemplate: "%s - BitcartCC Official Blog",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
-        hid: 'description',
-        name: 'description',
-        content: 'BitcartCC Official Blog'
-      }
+        hid: "description",
+        name: "description",
+        content: "BitcartCC Official Blog",
+      },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
   /*
    ** Global CSS
@@ -42,16 +42,16 @@ export default {
    */
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module',
+    "@nuxtjs/eslint-module",
     // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
-    '@nuxtjs/tailwindcss'
+    "@nuxtjs/tailwindcss",
   ],
   /*
    ** Nuxt.js modules
    */
   modules: [
     // Doc: https://github.com/nuxt/content
-    '@nuxt/content'
+    "@nuxt/content",
   ],
   /*
    ** Content module configuration
@@ -60,9 +60,9 @@ export default {
   content: {
     markdown: {
       prism: {
-        theme: 'prism-themes/themes/prism-material-oceanic.css'
-      }
-    }
+        theme: "prism-themes/themes/prism-material-oceanic.css",
+      },
+    },
   },
   /*
    ** Build configuration
@@ -76,9 +76,9 @@ export default {
           enforce: "pre",
           test: /\.(js|vue)$/,
           loader: "eslint-loader",
-          exclude: /(node_modules)/
+          exclude: /(node_modules)/,
         })
       }
-    }
-  }
+    },
+  },
 }
